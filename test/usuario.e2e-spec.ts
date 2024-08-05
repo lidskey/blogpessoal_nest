a50ee4e436c45bcf4d11f5ad4cba17aafe4c0d28
Liara Cristina <lidskey@hotmail.com>
2024-08-05 17:29:49 -0300

    test: teste do crud de usuário

---
+++ test/usuario.e2e-spec.ts
@@ -0,0 +1,111 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+describe('Testes dos Módulos Usuário e Auth (e2e)', () => {
+  let token: any;
+  let usuarioId: any;
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot({
+        type: "sqlite",
+        database: ":memory:",
+        entities: [__dirname + "./../src/**/entities/*.entity.ts"],
+        synchronize: true,
+        dropSchema: true
+      }),
+        AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("01 - Deve Cadastrar um novo Usuário", async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/usuarios/cadastrar')
+      .send({
+        nome: 'Root',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+        foto: ' x ',
+      })
+      .expect(201);
+    
+    usuarioId = resposta.body.id;
+  })
+
+  it("02 - Não Deve Cadastrar um Usuário Duplicado", async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/usuarios/cadastrar')
+      .send({
+        nome: 'Root',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+        foto: ' x ',
+      })
+      .expect(400);
+
+
+  })
+
+  it("03 - Deve Autenticar um novo Usuário(Login)", async () => {
+    const resposta = await request(app.getHttpServer())
+      .post('/usuarios/logar')
+      .send({
+        
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+       
+      })
+      .expect(200);
+
+    token = resposta.body.token;
+  })
+
+  it("04 - Deve Listar todos os Usuários", async () => {
+    const resposta = await request(app.getHttpServer())
+      .get('/usuarios/all')
+      .set('Authorization', `${token}`)
+
+      .expect(200);
+  })
+
+  it("05 - Deve Atualizar os dados do Usuário", async () => {
+    const resposta = await request(app.getHttpServer())
+      .put('/usuarios/atualizar')
+      .set('Authorization', `${token}`)
+      .send({
+        id: usuarioId,
+        nome: 'Administrador do Sistema',
+        usuario: 'root@root.com',
+        senha: 'rootroot',
+        foto: ' foto.jpg ',
+      })
+      .expect(200)
+      .then(resposta => {
+        expect("Administrador do Sistema").toEqual(resposta.body.nome);
+      })
+
+    
+  })
+
+  it("06 - Deve Retornar o Id do Usuário", async () => {
+    return request(app.getHttpServer())
+      .get(`/usuarios/${usuarioId}`)
+      .set('Authorization', `${token}`)
+      .send({})
+      .expect(200);
+   
+  });
+
+});
