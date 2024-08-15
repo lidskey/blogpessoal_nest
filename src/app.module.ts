4e05fa7ab4eca042fec1d856d203aaa4d59296a8
Liara Cristina <lidskey@hotmail.com>
2024-08-15 20:07:30 -0300

    refactor: importações não usadas foram apagadas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { UsuarioService } from './usuario/services/usuarios.service';
 import { ConfigModule } from '@nestjs/config';
 import { ProdService } from './data/services/prod.service';
 
