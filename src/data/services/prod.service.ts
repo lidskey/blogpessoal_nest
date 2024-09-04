218d02058b01ab52f62d6db91495b9ff86a80115
Liara Cristina <lidskey@hotmail.com>
2024-09-04 15:08:32 -0300

    fixes

--- src/data/services/prod.service.ts
+++ src/data/services/prod.service.ts
@@ -14,7 +14,7 @@ export class ProdService implements TypeOrmOptionsFactory {
             ssl: {
                 rejectUnauthorized: false,
             },
-            synchronize: false,
+            synchronize: true,
             autoLoadEntities: true,
         };
     }
